54e5fa813f98cbdd3efc5ceb900d59ec61db4cd2
berupon <berunpon@gmail.com>
2017-11-05 19:17:13 +0900

    delete old impl

--- serialization/strreverse.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void strreverse(char* begin, char* end);
-void wstrreverse(wchar_t* begin, wchar_t* end);
--- vsproj/readme.txt
+++
@@ -1,1 +0,0 @@
-'Microsoft Visual C++ Compiler Nov 2012 CTP' is for testing purposes only.
